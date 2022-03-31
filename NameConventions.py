4784c0713b913c42c602ef0c815b87abfaf30df8
Ali <newlifeforam@gmail.com>
2022-03-31 22:39:16 +0430

    hi

---
+++ NameConventions.py
@@ -0,0 +1,15 @@
+# conditional logic
+"""Comparison between thos subject which we have"""
+## REVIEW:
+print("Enter your name: ")
+my_name = input()
+
+#print(f"my name is {my_name.title()}.")
+
+# conditional
+userRank = input()
+
+if userRank == 6:
+    print("You Win!")
+else:
+    print("you die.")
